77125e9ae6c3cf69c4c83feb99d213c66d41f873
affederaffe <68356204+affederaffe@users.noreply.github.com>
2020-07-28 00:04:26 +0200

    Add files via upload

---
+++ Deeplcs/Paths.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deeplcs
+{
+    class Paths
+    {
+        public string Driver { get; set; }
+        public string Browser { get; set; }
+    }
+}
