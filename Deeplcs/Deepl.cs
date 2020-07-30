604be076ef283c68541dd670e954d00a68efb5ac
affederaffe <68356204+affederaffe@users.noreply.github.com>
2020-07-31 01:54:28 +0200

    Add files via upload

---
+++ Deeplcs/Deepl.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Collections.Generic;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using Newtonsoft.Json;
+
+namespace Deeplcs
+{
+    public class Deepl
+    {
+        private static string[] Json()
+        {
+            if (!File.Exists("paths.json"))
+            {
+                Paths paths = new Paths();
+                Console.WriteLine("Location of your browser: ");
+                paths.Browser = Console.ReadLine();
+                Console.WriteLine("Path to your driver: ");
+                paths.Driver = Console.ReadLine();
+                using (StreamWriter file = File.CreateText("paths.json"))
+                {
+                    JsonSerializer serializer = new JsonSerializer();
+                    serializer.Serialize(file, paths);
+                }
+            }
+            Paths Paths = JsonConvert.DeserializeObject<Paths>(File.ReadAllText("paths.json"));
+            string Browser = Paths.Browser;
+            string Driver = Paths.Driver;
+            return new string[] { Browser, Driver };
+        }
+
+        public static string[] Translate(string targetLang, string[] stringArray, string[] srcTextArray = null, string[] targetTextArray = null, string srcLang = "auto", bool headless = true)
+        {
+            string[] paths = Json();
+            string Browser = paths[0];
+            string Driver = paths[1];
+            ChromeOptions options = new ChromeOptions();
+            options.BinaryLocation = Browser;
+            options.AddArguments("--disable-extensions", "--window-size=1920,1080", "--start-maximized");
+            if (headless)
+            {
+                options.AddArguments("--headless");
+            }
+            IWebDriver driver = new ChromeDriver(Driver, options);
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(10));
+
+            List<string> outputList = new List<string>();
+
+            driver.Navigate().GoToUrl(@"https://deepl.com/translator");
+            if (srcLang != "auto")
+            {
+                var srcLanguageMenuButtonElement = driver.FindElement(By.XPath("//*[@id='dl_translator']/div[1]/div[3]/div[1]/div/button"));
+                srcLanguageMenuButtonElement.Click();
+                Thread.Sleep(500);
+                var srcLanguageButtonElements = driver.FindElements(By.CssSelector(string.Format("button[dl-lang = {0}", srcLang)));
+                srcLanguageButtonElements[0].Click();
+            }
+            var targetLanganguageMenuButtonElement = driver.FindElement(By.XPath("//*[@id='dl_translator']/div[1]/div[4]/div[1]/div[1]/div[1]/button"));
+            targetLanganguageMenuButtonElement.Click();
+            Thread.Sleep(500);
+            var targetLanguageButtonElements = driver.FindElements(By.CssSelector(string.Format("button[dl-lang = {0}", targetLang)));
+            targetLanguageButtonElements[1].Click();
+            if (srcTextArray != null)
+            {
+                var glossaryElement = driver.FindElement(By.XPath("//*[@id='glossaryButton']/button"));
+                glossaryElement.Click();
+                try
+                {
+                    var element = wait.Until(drv => drv.FindElement(By.XPath("//*[@id='glossaryButton']/button")));
+                }
+                finally
+                {
+                    for (int i = 0; i < srcTextArray.Length; i++)
+                    {
+                        var srcTextElement = driver.FindElement(By.XPath("//*[@id='glossaryEditor']/div[4]/form/div[1]/input[1]"));
+                        srcTextElement.SendKeys(srcTextArray[i]);
+                        var targetTextElement = driver.FindElement(By.XPath("//*[@id='glossaryEditor']/div[4]/form/div[1]/input[2]"));
+                        targetTextElement.SendKeys(targetTextArray[i]);
+                        var glossaryAcceptElement = driver.FindElement(By.XPath("//*[@id='glossaryEditor']/div[4]/form/div[1]/button[2]"));
+                        glossaryAcceptElement.Click();
+                    }
+                    var glossaryCloseElement = driver.FindElement(By.XPath("//*[@id='glossaryEditor']/div[2]/div[3]"));
+                    glossaryCloseElement.Click();
+                }
+            }
+            for (int i = 0; i < stringArray.Length; i++)
+            {
+                try
+                {
+                    var clearElement = driver.FindElement(By.XPath("//*[@id='dl_translator']/div[1]/div[3]/div[2]/button"));
+                    clearElement.Click();
+                }
+                catch
+                {
+                    { }
+                }
+                var textInputElement = driver.FindElement(By.XPath("//*[@id='dl_translator']/div[1]/div[3]/div[2]/div/textarea"));
+                textInputElement.SendKeys(stringArray[i]);
+                while (driver.FindElement(By.XPath("//*[@id='dl_translator']/div[1]/div[6]")).GetAttribute("class") == "lmt__mobile_share_container lmt__mobile_share_container--inactive")
+                {
+                    Thread.Sleep(100);
+                }
+                Thread.Sleep(100);
+                var outputElements = driver.FindElements(By.CssSelector("button[class='lmt__translations_as_text__text_btn']"));
+                outputList.Add(outputElements[0].GetAttribute("innerHTML"));
+            }
+            driver.Quit();
+            return outputList.ToArray();
+        }
+    }
+}
